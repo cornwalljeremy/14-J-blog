e40215627e9f630d7484d94eca1faf12b8a689f6
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-12-14 16:56:30 -0700

    change

--- models/Index.js
+++ models/Index.js
@@ -1,24 +1,24 @@
 const User = require("./User");
 const Post = require("./Post");
 
-const Comment = require("./Comment");
+const Comment = require("./Comment"); 
 
 // create associations
 User.hasMany(Post, {
   foreignKey: "user_id",
-});
+}); 
 
 User.hasMany(Comment, {
   foreignKey: "user_id",
-});
+}); 
 
 Post.belongsTo(User, {
   foreignKey: "user_id",
-});
+}); 
 
 Post.hasMany(Comment, {
   foreignKey: "post_id",
-});
+}); 
 
 
 
