15fa294844435a467460514e0898ed2753141796
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-12-15 11:45:17 -0700

    again nothing

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    host: 'localhost',
     dialect: 'mysql',
     port: 3306,
     
