bfd60a09b88122c0c858e9300cfe416c0eb7f494
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-12-15 12:13:31 -0700

    dotty dot

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { route } = require(".");
-const { User, Post, Comment } = require("../../models");
+const { User, Post, Comment } = require("/../models");
 
 router.get("/", (req, res) => {
   User.findAll({
